438a57f6f8ee59f26650f5ab815d144af8632567
AbuLawiQA <qaabulawi2017@gmail.com>
2025-08-08 21:26:57 +0300

    UpdatedReport

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -3,6 +3,7 @@ package utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
     private static ExtentReports extent;
@@ -11,6 +12,11 @@ public class ExtentManager {
         if (extent == null) {
             String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
             ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+
+            sparkReporter.config().setDocumentTitle("Automation Report");
+            sparkReporter.config().setReportName("End-to-End Tests");
+            sparkReporter.config().setTheme(Theme.STANDARD);
+
 
             extent = new ExtentReports();
             extent.attachReporter(sparkReporter);
